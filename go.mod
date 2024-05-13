0bd6e2392c5be961a1c9ae8e506099fa73b4df7c
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-05-13 22:02:28 +0300

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.22.0
+
+require fyne.io/fyne/v2 v2.4.5 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func makeUI(button *widget.Button) *fyne.Container {
+	return container.New(layout.NewGridLayout(0),
+		button,
+	)
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Dash")
+
+	button := widget.NewButton("Press Me ;)", func() {
+		fmt.Println("Hello world")
+	})
+
+	w.SetContent(makeUI(button))
+
+	a.Run()
+	w.Show()
+}
